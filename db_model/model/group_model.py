dd668d295c34490442ed0a0a61d7ca545140a54b
lin <903784307@qq.com>
2019-03-03 02:34:52 +0800

    增加数据库模型文件

---
+++ db_model/model/group_model.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# @author  : lin
+# @time    : 19-3-3
+
+from db_model.model import BaseModel
+from peewee import CharField, BigIntegerField
+
+
+class GroupModel(BaseModel):
+    group_id = BigIntegerField(primary_key=True)
+    group_name = CharField()
+
+    class Meta:
+        db_table = 'group'
---
+++ db_model/model/predict_model_model.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# @author  : lin
+# @time    : 19-3-3
+
+from db_model.model import BaseModel
+from peewee import CharField, BigIntegerField
+
+
+class PredictModelModel(BaseModel):
+    model_id = BigIntegerField(primary_key=True)
+    model_information = CharField()
+
+    class Meta:
+        db_table = 'predict_model'
