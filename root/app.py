5fc99d6a6216a08e01fbd7aa7e9989d389b95980
lin <903784307@qq.com>
2019-04-02 12:19:42 +0800

    root下接口函数等修改

--- root/app.py
+++ root/app.py
@@ -1,18 +1,19 @@
 # coding: utf-8
 # @author  : lin
 # @time    : 19-2-28
-from . import root_app
-from flask import request
+from . import dao
+from flask import request, Blueprint
 from lib.http_response_code import response
 from lib.decorator import catch_error
 from db_model.model_dao import UserModelDao
 import json
 
+root_app = Blueprint('root_app', __name__)
 
 
-@root_app.route('add_master', methods=['POST'])
+@root_app.route('add_manager', methods=['POST'])
 @catch_error
-def add_master():
+def add_manager():
     """
     添加管理员
     :return:
@@ -30,9 +31,9 @@ def add_master():
     return json.dumps(response_data, ensure_ascii=False)
 
 
-@root_app.route('delete_master', methods=['POST'])
+@root_app.route('delete_manager', methods=['POST'])
 @catch_error
-def delete_master():
+def delete_manager():
     """
     删除管理员
     :return:
@@ -48,3 +49,105 @@ def delete_master():
         response_data = response[20101]
 
     return json.dumps(response_data, ensure_ascii=False)
+
+
+@root_app.route('get_info', methods=['GET'])
+@catch_error
+def get_user_info():
+    token = request.headers.get('token')
+    user_id = dao.get_user_id_from_token(token)
+    print(user_id)
+    user_name = dao.get_user_name_from_id(user_id)
+    # uid =
+    res = {
+        'avator': 'ME',   # 头像
+        'name': user_name,
+        'user_id': user_id,
+        'access': ['super_admin']
+    }
+    return json.dumps({'code': 200, 'msg': 'Success', 'data': res})
+
+
+@root_app.route('login', methods=['POST'])
+@catch_error
+def user_login():
+    """
+    获取api调用token
+    :return:
+    """
+    req_json = request.json
+    user_name = req_json["user_name"]
+    user_pwd = req_json["user_pwd"]
+
+    if not (user_name and user_pwd):
+        # 参数缺失
+        return json.dumps(response[20101])
+
+    user_id = UserModelDao.check_user_and_user_pwd(user_name, user_pwd)
+    if not dao.is_user_root(user_id):
+        # 校验不通过
+        return json.dumps(response[20201]) if user_id == -1 else json.dumps(response[20202])
+
+    token = dao.get_one_token()
+    dao.set_user_id_token(token, user_id)
+    response_data = {"token": token, 'user_id': user_id}
+    response_data.update(response[200])
+    return json.dumps(response_data)
+
+
+@root_app.route('logout', methods=['POST'])
+@catch_error
+def user_logout():
+    """
+    取消掉token
+    :return:
+    """
+    token = request.headers.get('token')
+    dao.delete_one_token(token)
+    user_id = dao.get_user_id_from_token(token)
+    return json.dumps(response[200])
+
+
+@root_app.route('get_user_mount', methods=['GET'])
+@catch_error
+def get_user_mount():
+    """
+    得到用户数量
+    :return:
+    """
+    all_user_list = UserModelDao.query_user(0)
+    data = {'root': 0, 'manager': 0, 'user': 0}
+    for user_model in all_user_list:
+        if user_model.group_id == 1:
+            data['user'] += 1
+        elif user_model.group_id == 2:
+            data['manager'] += 1
+        elif user_model.group_id == 3:
+            data['root'] += 1
+    response_data = {'data': data}
+    response_data.update(response[200])
+    return json.dumps(response_data)
+
+
+@root_app.route('get_user_data', methods=['POST'])
+@catch_error
+def get_user_data():
+    page_size = request.json['page_size']
+    page = request.json['page']
+    group_id = request.json['group_id']
+    all_user_list = UserModelDao.query_user(3, group_id=group_id)
+    all_user_list = [i for i in all_user_list]
+    user_list = all_user_list[(page - 1) * page_size: page * page_size]
+    user_data = []
+    for user in user_list:
+        user_data.append({'user_name': user.user_name, 'user_pwd': user.user_pwd,
+                          'user_state': user.user_state, 'email': user.email})
+    data = {'count': len(all_user_list), 'user_data': user_data}
+    return json.dumps({'code': 200, 'msg': 'Success', 'data': data})
+
+
+@root_app.route('get_user_ui', methods=['GET'])
+@catch_error
+def return_ui_config():
+    columns = UserModelDao.get_ui_config()
+    return json.dumps({'code': 200, 'msg': 'Success', 'data': columns})
--- root/dao.py
+++ root/dao.py
@@ -1,3 +1,85 @@
 # coding: utf-8
 # @author  : lin
 # @time    : 19-2-28
+
+from db_model.model_dao import UserModelDao, GroupPowerModelDao
+# from . import config
+from config.db_config import redis_client
+from lib.MD5_encrypt import md5_encrypt
+from time import time
+from datetime import datetime
+
+user_id_token_key = "user_id_token_key_"  # token 与 user_id记录的redis key前缀
+user_token_timeout = 3600 * 24  # token有效期
+timer_token = "timer_"  # 利用redis计数器实现自增id，防止高并发下token重复
+
+
+def get_user_id_from_token(token):
+    """
+    根据token获取用户id
+    :param token: token
+    :return:
+    """
+    if not token:
+        return 0
+    cache_key = user_id_token_key + token
+    d = redis_client.get(cache_key)
+    if d:
+        # 设置过期时间
+        redis_client.expire(cache_key, user_token_timeout)
+    return int(d) if d else -1
+
+
+def set_user_id_token(token, user_id):
+    """
+    记录token数据
+    :param token: token
+    :param user_id: 用户id
+    :return:
+    """
+    cache_key = user_id_token_key + token
+    redis_client.set(cache_key, user_id)
+    # 设置超时时间
+    redis_client.expire(cache_key, user_token_timeout)
+
+
+def get_one_token():
+    """
+    随机生成一个32位的字符串
+    :return:
+    """
+    timer_key = timer_token + datetime.now().strftime("%Y-%m-%d")
+    # 自增，防止高并发下重复
+    timer_id = redis_client.incr(timer_key)
+    redis_client.expire(timer_key, 3600 * 24)
+    d = str(time()) + "_" + str(timer_id)
+    return md5_encrypt(d)
+
+
+def get_user_name_from_id(user_id):
+    """
+    由id得name
+    :param user_id:
+    :return:
+    """
+    user = UserModelDao.find_by_user_id(user_id)
+    if not (user and user.user_name):
+        return None
+    return user.user_name
+
+
+def is_user_root(user_id):
+    """
+    查看一个user是否root用户
+    :param user_id:
+    :return:
+    """
+    user = UserModelDao.find_by_user_id(user_id)
+    if not (user and user.group_id == 3):
+        return False
+    return True
+
+
+def delete_one_token(token):
+    cache_key = user_id_token_key + token
+    redis_client.delete(cache_key)
